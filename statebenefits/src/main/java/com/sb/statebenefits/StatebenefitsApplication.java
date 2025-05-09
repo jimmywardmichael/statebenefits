ae4f21b599656606afbcc8e1f646303fcbe0d024
jimmywardmichael <jimmywardmichael@gmail.com>
2025-05-09 16:08:53 -0500

    initial commit

---
+++ statebenefits/src/main/java/com/sb/statebenefits/StatebenefitsApplication.java
@@ -0,0 +1,13 @@
+package com.sb.statebenefits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatebenefitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatebenefitsApplication.class, args);
+	}
+
+}
---
+++ statebenefits/src/test/java/com/sb/statebenefits/StatebenefitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.statebenefits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatebenefitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
